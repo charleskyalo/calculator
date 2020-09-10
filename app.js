866770e15ea5c1df9b193c828046f9839666a8ca
charles kyalo <charleskyalo689@gmail.com>
2020-09-10 19:06:04 +0300

    display the  numbers to the screen

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 /* what is previously on the screen */
 let runningTotal = 0;
-
 /* what is on the screen  */
-
-
 let buffer = "0";
-
 /*Keep track of the previous operator  */
-
 let previousOperator;
-
 const screen = document.querySelector('.screen');
-
 /* what happens wen a function is clicked ? */
-
 function buttonClick(value) {
     if (isNaN(value)) {
         handleSymbol(value);
     } else {
         handleNumber(value);
-
     }
 }
 
@@ -28,16 +19,15 @@ function handleSymbol(symbol) {
 }
 
 function handleNumber(numberString) {
-
     if (buffer === "0") {
-        buffer = "0";
+        buffer = numberString;
     } else {
         buffer += numberString;
-
     }
+    screen.innerText = buffer;
 }
 
-handleNumber();
+
 /* intial state  of  the calc */
 function init() {
     document.querySelector('.calc-buttons')
