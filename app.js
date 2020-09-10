4d6574c03ae67c8875f46581233bb4751aa57d5a
charles kyalo <charleskyalo689@gmail.com>
2020-09-10 17:48:47 +0300

    add app.js to the project and add code to handle numbers  in the calculator

---
+++ app.js
@@ -0,0 +1,49 @@
+/* what is previously on the screen */
+let runningTotal = 0;
+
+/* what is on the screen  */
+
+
+let buffer = "0";
+
+/*Keep track of the previous operator  */
+
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+/* what happens wen a function is clicked ? */
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+
+    }
+}
+
+function handleSymbol(symbol) {
+
+}
+
+function handleNumber(numberString) {
+
+    if (buffer === "0") {
+        buffer = "0";
+    } else {
+        buffer += numberString;
+
+    }
+}
+
+handleNumber();
+/* intial state  of  the calc */
+function init() {
+    document.querySelector('.calc-buttons')
+        .addEventListener('click', function (event) {
+            buttonClick(event.target.innerText);
+        });
+}
+
+init();
