5e59742b0760b367b049949b9decc0c997f2ea71
charles kyalo <charleskyalo689@gmail.com>
2020-09-12 12:55:44 +0300

    add calcs functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let runningTotal = 0;
 /* what is on the screen  */
 let buffer = "0";
 /*Keep track of the previous operator  */
-let previousOperator;
+let previousOperator = null;
 const screen = document.querySelector('.screen');
 /* what happens wen a function is clicked ? */
 function buttonClick(value) {
@@ -16,10 +16,72 @@ function buttonClick(value) {
 }
 
 function handleSymbol(symbol) {
-    if (symbol === 'c') {
-        buffer = "0";
-        runningTotal = 0;
+    console.log('Handle Symbol:', symbol);
+    switch (symbol) {
+        case 'c':
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                // you need two numbers to  do math.
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "+":
+        case "-":
+        case "×":
+        case "÷":
+            handleMath(symbol);
+            break;
     }
+}
+
+
+
+/* handle Math */
+function handleMath(symbol) {
+    if (buffer === "0") {
+        // do nothing
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    buffer = "0";
+}
+
+
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+
+    console.log("running total", runningTotal);
 }
 
 function handleNumber(numberString) {
